663c5d6270fdf1a10329cd99f9fece8e8d8d64fa
Korbinian Maag <korbinian.maag@gmail.com>
2017-08-06 10:48:29 +0200

    Erstennen von Playerscript
    Aufbau der Gravitationänderung und controller input

---
+++ Assets/C#/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //      1 UP
+    // 4 LEFT    2 RIGHT
+    //      3 DOWN
+    //Ändert die Gravitation nach der dementsprechenden Nummer
+    public int gDirection = 3;
+
+	// Use this for initialization
+	void Start () {
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Nimmt den Input und 
+        InputDirector();
+        GravityDirector();
+
+    }
+
+    void InputDirector()
+    {
+
+    }
+
+    void GravityDirector()
+    {
+        switch (gDirection)
+        {
+            case 1:
+                Physics2D.gravity = new Vector2(0, 13f);
+                break;
+            case 2:
+                Physics2D.gravity = new Vector2(-13f, 0);
+                break;
+            case 3:
+                Physics2D.gravity = new Vector2(0, -13f);
+                break;
+            case 4:
+                Physics2D.gravity = new Vector2(13f, 0);
+                break;
+        }
+
+    }
+
+
+}
