1826be9cfb7e13485fc6ec449f81aa49b97a76b0
Korbinian Maag <korbinian.maag@gmail.com>
2017-08-13 11:47:18 +0200

    Lässt Text unsichtbar machen bei Spielstart

---
+++ Assets/C#/HideMyText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMyText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<CanvasGroup>().alpha = 0f;
+	}
+}
