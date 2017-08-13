662edbd1c1cc21610a596493f2d6bad0736c6ef5
Korbinian Maag <korbinian.maag@gmail.com>
2017-08-13 11:46:37 +0200

    Erstellen von Prefabs und einrichten von Help Scripts

---
+++ Assets/C#/TriggerText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerText : MonoBehaviour {
+
+    //Können Alle Texte definiert werden
+    public GameObject[] Text;
+    public bool oneAtATime = true;
+    public float timeBetween = 1f;
+    public float alphaSpeed = 0.1f;
+
+    bool used = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!used)
+        {
+            used = true;
+
+            if (oneAtATime)
+            {
+                StartCoroutine(OneAtATime());
+            } else
+            {
+                foreach (GameObject t in Text)
+                {
+                    StartCoroutine(CanvasGroupAlpha(t));
+                }
+            }
+        }
+    }
+
+    IEnumerator OneAtATime()
+    {
+        foreach(GameObject t in Text)
+        {
+            StartCoroutine(CanvasGroupAlpha(t));
+            yield return new WaitForSeconds(timeBetween);
+        }
+
+    }
+
+    IEnumerator CanvasGroupAlpha(GameObject t)
+    {
+        var alpha = t.GetComponent<CanvasGroup>().alpha;
+
+        while(alpha < 1)
+        {
+            alpha = alpha + 0.1f;
+            yield return new WaitForSeconds(alphaSpeed);
+        }
+    }
+
+}
